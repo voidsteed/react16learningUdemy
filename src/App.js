8cf198b1c0ba95305eaa7598070d079992c5171e
Yujun Liu <yujun.liu@vanderbilt.edu>
2018-05-15 00:33:32 -0700

    section 3 chap67

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -69,18 +69,18 @@ class App extends Component {
       style.backgroundColor = "red";
     }
 
-    let classes = [];
+    const assignedClasses  = [];
     if (this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if (this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
       <h1>Hi im a react app</h1>
-      <p className={classes.join(' ')}>this is really working</p>
+      <p className={assignedClasses.join(' ')}>this is really working</p>
       <button 
         style={style}
         onClick={this.togglePersonsHandler}>Toggle Persons</button>

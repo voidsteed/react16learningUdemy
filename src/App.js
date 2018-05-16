b0307747616da5ef977d31427e56eeb1049e624c
Yujun Liu <yujun.liu@vanderbilt.edu>
2018-05-16 00:32:17 -0700

    section 5 lecture 77 restore to previous state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -50,12 +49,12 @@ class App extends Component {
         <div>
           {
             this.state.persons.map((person, index) => {
-              return <ErrorBoundary key={person.id}><Person 
+              return <Person 
                 
                 click={() => this.deletePersonHandler(index)}
                 name={person.name}
                 age={person.age}
-                changed={(event) => this.nameChangedHandler(event, person.id)}/></ErrorBoundary>
+                changed={(event) => this.nameChangedHandler(event, person.id)}/>
             })
           }
 

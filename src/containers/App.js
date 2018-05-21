223976d8df850c3c38fb7e8035a2136a01202bc5
Yujun Liu <dreamzack@gmail.com>
2018-05-20 22:22:18 -0700

    section 5 lecture 80 refactor file structures way better

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
+import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
   state = {
     persons: [
-      {id: '1', name: 'Max', age: 28},
-      {id: '2', name: 'Manu', age: 29},
-      {id: '3', name: 'Stephanie', age: 26}
+      { id: '1', name: 'Max', age: 28 },
+      { id: '2', name: 'Manu', age: 29 },
+      { id: '3', name: 'Stephanie', age: 26 }
     ],
-    showPersons: false
+    showPersons: false,
   }
 
-   nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex(p => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
       return p.id === id;
-    })
+    });
     const person = {
-      ...this.state.persons[personIndex]
+      ...this.state.persons[personIndex],
     }
-    person.name = event.target.value
+    person.name = event.target.value;
 
     const persons = [...this.state.persons];
     persons[personIndex] = person;
